805f3c4553fe2d232728f68ec6e65e568887cf2b
Harrison Lee <laharrari@gmail.com>
2019-02-15 22:44:36 -0800

    Issues with master branch, attempting to resolve.

--- gameengine.js
+++ gameengine.js
@@ -71,28 +71,11 @@ GameEngine.prototype.startInput = function () {
         if (SCENE_MANAGER.insideMenu) {
             SCENE_MANAGER.menuSelection(x, y);
         } else {
-            if (!that.insideMenu && playerPick == 0) {
+            if (that.playerPick == 0) {
                 // Projectile
                 var projectile = new Projectile(GAME_ENGINE, AM.getAsset("./img/fireball.png"),
                     myPlayer.x - (myPlayer.width / 2), myPlayer.y - (myPlayer.height / 2), x, y);
                 GAME_ENGINE.addEntity(projectile);
-                GAME_ENGINE.addProjectileEntity(projectile);
-            }
-
-            else if (!that.insideMenu && playerPick == 1) {
-                // Projectile
-                var projectile = new Projectile(GAME_ENGINE, AM.getAsset("./img/fireball.png"),
-                    myPlayer.x - (myPlayer.width / 2), myPlayer.y - (myPlayer.height / 2), x, y);
-                GAME_ENGINE.addEntity(projectile);
-                GAME_ENGINE.addProjectileEntity(projectile);
-            }
-
-            else if (!that.insideMenu && playerPick == 2) {
-                // Projectile
-                var projectile = new Projectile(GAME_ENGINE, AM.getAsset("./img/fireball.png"),
-                    myPlayer.x - (myPlayer.width / 2), myPlayer.y - (myPlayer.height / 2), x, y);
-                GAME_ENGINE.addEntity(projectile);
-                GAME_ENGINE.addProjectileEntity(projectile);
             }
         }
     }, false);
@@ -229,7 +212,7 @@ function Entity(game, x, y) {
     this.removeFromWorld = false;
 }
 
-Entity.prototype.update = function () { }
+Entity.prototype.update = function () {}
 
 Entity.prototype.draw = function (ctx) {
     if (this.game.showOutlines && this.radius) {
