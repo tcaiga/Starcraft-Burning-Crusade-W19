adc835212f4bbab0b44f2dd3715409cca2324a52
terencecaiga <terencecaiga@gmail.com>
2019-02-28 17:55:18 -0800

    fix door flickering and background still needs to be fixed

--- gameengine.js
+++ gameengine.js
@@ -285,7 +285,7 @@ GameEngine.prototype.addEntity = function (entity) {
         this.entities[3].push(entity);
     } else if (entity instanceof Trap) {
         this.entities[2].push(entity);
-    } else if (entity instanceof Menu) {
+    } else if (entity instanceof Menu || entity instanceof Background) {
         this.entities[0].push(entity);
     } else {
         this.entities[1].push(entity);
@@ -333,7 +333,8 @@ GameEngine.prototype.draw = function () {
     for (let i = 0; i < this.entities.length; i++) {
         for (let j = 0; j < this.entities[i].length; j++) {
             var entity = this.entities[i][j];
-            if (!entity.removeFromWorld && (i <= 1 || (entity.x >= CAMERA.x && entity.x <= CAMERA.x + canvasWidth &&
+            if (!entity.removeFromWorld && (i < 1 || 
+                (entity.x >= CAMERA.x && entity.x <= CAMERA.x + canvasWidth &&
                 entity.y >= CAMERA.y && entity.y <= CAMERA.y + canvasHeight))) {
                 entity.draw(this.ctx);
             }
@@ -346,7 +347,7 @@ GameEngine.prototype.update = function () {
     for (let i = 0; i < this.entities.length; i++) {
         for (let j = 0; j < this.entities[i].length; j++) {
             var entity = this.entities[i][j];
-            if (!entity.removeFromWorld && (i <= 1 || (entity.x >= CAMERA.x && entity.x <= CAMERA.x + canvasWidth &&
+            if (!entity.removeFromWorld && (i < 1 || (entity.x >= CAMERA.x && entity.x <= CAMERA.x + canvasWidth &&
                 entity.y >= CAMERA.y && entity.y <= CAMERA.y + canvasHeight))) {
                 entity.update();
             }
