d37edeb96c7b21ef5d6867da746246790438cfe8
Harrison Lee <laharrari@gmail.com>
2019-03-02 01:11:22 -0800

    Updated marine idle death.

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ function Player(runSheet, shootSheet, deathSheet, xOffset, yOffset) {
     this.castTime = 0;
     this.isStunned = false;
     this.dead = false;
-    this.baseMaxMovespeed = 2;
+    this.baseMaxMovespeed = 2.5;
     this.maxMovespeedRatio = 1;
     this.maxMovespeedAdj = 0;
     this.actualSpeed = (this.baseMaxMovespeed * this.maxMovespeedRatio + this.maxMovespeedAdj);
