c63400b10353c9f980d1e0812a55c00dd290d608
terencecaiga <terencecaiga@gmail.com>
2019-03-14 19:42:20 -0700

    fixed shoot direction bug

--- main.js
+++ main.js
@@ -114,9 +114,9 @@ Player.prototype.draw = function () {
                         this.xScale = -1;
                         xValue = -this.x - this.width;
                     }
-                    if (this.shootDirection === "left" || this.shootDirection === "right") {
+                    if (this.shootDirection  === "left" || this.shootDirection  === "right") {
                         this.animationShootSide.drawFrame(GAME_ENGINE.clockTick, GAME_ENGINE.ctx, xValue, this.y);
-                    } else if (this.shootDirection === "up") {
+                    } else if (this.shootDirection  === "up") {
                         this.animationShootUp.drawFrame(GAME_ENGINE.clockTick, GAME_ENGINE.ctx, xValue, this.y);
                     } else {
                         this.animationShootDown.drawFrame(GAME_ENGINE.clockTick, GAME_ENGINE.ctx, xValue, this.y);
@@ -211,11 +211,6 @@ Player.prototype.update = function () {
             }
         }
 
-        //casts the selected spell if it is off cooldown
-        if (spellCast || this.abilityCD[selectSpell] <= 0) {
-            this.castSpell(selectSpell);
-        }
-
         if (GAME_ENGINE.keyUp === true) {
             //changes the direction to shoot the projectile
             this.shootDirection = "up";
@@ -227,13 +222,18 @@ Player.prototype.update = function () {
         } else if (GAME_ENGINE.keyRight === true) {
             this.shootDirection = "right";
             this.animationIdle = this.animationRunSide;
-        } else {
+        } else if (GAME_ENGINE.keyDown === true){
             this.shootDirection = "down";
             this.animationIdle = this.animationRunDown;
         }
         this.lastShootDirection = this.shootDirection;
+        //casts the selected spell if it is off cooldown
+        if (spellCast || this.abilityCD[selectSpell] <= 0) {
+            this.castSpell(selectSpell);
+        }
 
         if (GAME_ENGINE.shoot && this.currentAmmo > 0 && !GAME_ENGINE.reload) {
+
             if (this.shootCounter >= this.maxShootCounter) {
                 var projectile = new Projectile(AM.getAsset("./img/terran/bullet.png"),
                     myPlayer.x + 15,
